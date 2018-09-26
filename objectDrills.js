992c7a0763e564bf7d74791a2f3ec31ffc2ac811
jeff oriente <jdanieloriente@gmail.com>
2018-09-26 12:24:24 -0400

    Did 2 object drills

---
+++ objectDrills.js
@@ -0,0 +1,43 @@
+// Object creator
+// In this drill, you need to write code for the createMyObject function so that it returns an object with the following key value pairs:
+
+// foo => 'bar'
+// answerToUniverse => 42
+// olly olly => 'oxen free'
+// sayHello => a function that returns the string 'hello'
+
+// function createMyObject() {
+// 	return {
+// 		foo: 'bar',
+// 		answerToUniverse: 42,
+// 		'olly olly': 'oxen free',
+// 		sayHello: function(){return 'hello';}
+// 	}
+// }
+
+// let testObject = createMyObject();
+// console.log(testObject);
+
+////////////////////////////////////////////////////////////////////////////
+
+// Object updater
+// Modify the updateObject function (which takes a single argument (obj)) so that it adds the following key/value pairs to obj and returns it:
+
+// foo => 'foo'
+// bar => 'bar'
+// bizz => 'bizz'
+// bang => 'bang'
+
+// function updateObject(obj) {
+// 	obj.foo = 'foo',
+// 	obj.bar = 'bar',
+// 	obj.bizz = 'bizz',
+// 	obj.bang = 'bang'
+// 	return obj;
+// }
+// let test = {};
+// console.log(updateObject(test));
+///////////////////////////////////////////////////////
+
+
+
